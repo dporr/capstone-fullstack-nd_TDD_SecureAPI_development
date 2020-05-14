85e53152b77d259b1dd3ee0a119fa983381d820b
dporr <dporras.mail@gmail.com>
2020-05-14 01:31:16 -0600

    Add env variables to setup script

---
+++ setup.sh
@@ -0,0 +1,5 @@
+export AUTH0_DOMAIN=dporras-fullstack-nd.auth0.com
+export API_AUDIENCE=fullstack-capstone-dporr
+export ALGORITHMS=['RS256']
+export CLIENT_ID=D60YWT6ejaKIW2CA62PYt62WGSxSnxUo
+export DATABASE_URL=postgres://xopnccimidaafn:$PASSWORD@ec2-35-171-31-33.compute-1.amazonaws.com:5432/dc4bl3kjohq6sq
